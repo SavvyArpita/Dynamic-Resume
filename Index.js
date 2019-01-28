e5d8d6942f2ec806fba33836151bc727acbd3b70
Arpita Singh <arpitaa.90@gmail.com>
2019-01-28 18:33:38 +0530

    Added the About me page animation

--- Index.js
+++ Index.js
@@ -1,4 +1,6 @@
 {
+	/* Home page animation starts */
+
 	setTimeout(() => document.body.classList.add('render'), 60);
 	const navdemos = Array.from(document.querySelectorAll('nav.demos > .demo'));
 	const total = navdemos.length;
@@ -25,4 +27,52 @@
 		}
 		navigate(linkEl);
 	});
+
+	/* Home page animation ends */
+
+	/* About me page animation starts */
+
+	// The about me Heading animation
+
+	$('.intro1').each(function(){
+		$(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+	});
+	  
+	anime.timeline({loop: false})
+	.add({
+		targets: '.intro1 .letter',
+		scale: [4,1],
+		opacity: [0,1],
+		translateZ: 0,
+		easing: "easeOutExpo",
+		duration: 950,
+		delay: function(el, i) {
+		return 70*i;
+		}
+	}).add({
+		targets: '.intro1',
+		opacity: 1,
+		easing: "easeOutExpo",
+		delay: 1000
+	});
+
+	// The about me description animation
+
+	let label = document.querySelector('.intro2 .letters');
+	charming(label);
+	let domletters= Array.from(label.querySelectorAll('span'));
+	applyAnime();
+	function applyAnime() {
+		anime.timeline({loop: false})
+		.add({
+			targets: domletters,
+			duration: 20,
+			delay: (t,i) => (i+5)*30,
+			easing: 'easeInOutExpo',
+			opacity: [0,1]
+		})
+	}
+
+	/* About me page animation ends */
+	
 }

f655b57a2fd298453258d1cdddcc13a9dcbfb584
Arpita Singh <arpitaa.90@gmail.com>
2019-02-06 18:53:15 +0530

    Added welcome page and disabled mouse wheeler action

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 {
+	$('body').bind("mousewheel", function() {
+		return false;
+	});
 	// When the user scrolls down 20px from the top of the document, show the button
 	window.onscroll = function() {scrollFunction()};
 
